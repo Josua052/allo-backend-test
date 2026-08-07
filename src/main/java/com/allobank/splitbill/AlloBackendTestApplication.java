e8daae94502026f63d938ebfe5d0c9a24ee9a093
Josua052 <josuaronaldo96@gmail.com>
2026-08-07 17:31:03 +0700

    chore: init spring boot project with core dependencies

---
+++ src/main/java/com/allobank/splitbill/AlloBackendTestApplication.java
@@ -0,0 +1,14 @@
+package com.allobank.splitbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main entry point for the Spring Boot application
+@SpringBootApplication
+public class AlloBackendTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlloBackendTestApplication.class, args);
+    }
+
+}
